84888ef8a32d780773c78c50fc9fd4084142187b
Denzel <120992039+jdenzel@users.noreply.github.com>
2025-09-27 12:22:34 -0400

    Created getUsers function

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -1,4 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+
 function UserProfile() {
+
+  const query = useQuery({
+    queryKey: ['users'],
+  })
+
   return ( 
     <>
     <h1>Hello</h1>
